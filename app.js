90547da9497d8517a5543b0c00247b5d42ec02eb
Artur <artur.krupa@icloud.com>
2022-07-05 22:10:05 +0200

    Add event listener to divs

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const divContainer = document.querySelector(".grid-container");
 
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement("div");
+const numberOfGrid = 256;
+
+for (let i = 0; i < numberOfGrid; i++) {
+  let div = document.createElement("div");
   divContainer.appendChild(div);
+
+  div.addEventListener("pointerenter", function () {
+    div.style.background = "red";
+  });
 }
