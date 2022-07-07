84cb3f7fc583434293306b89ee708f539879178d
Artur <artur.krupa@icloud.com>
2022-07-07 21:52:39 +0200

    Create a function and event listener for button

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const divContainer = document.querySelector(".grid-container");
+const sizeButton = document.querySelector(".grid-size-select");
+sizeButton.addEventListener("click", () => {
+  let numberOfGrid = prompt("how big is tyour grid");
+  renderCanvas(numberOfGrid ** 2);
+});
 
-const numberOfGrid = 256;
+let renderCanvas = (size) => {
+  for (let i = 0; i < size; i++) {
+    let div = document.createElement("div");
+    divContainer.appendChild(div);
 
-for (let i = 0; i < numberOfGrid; i++) {
-  let div = document.createElement("div");
-  divContainer.appendChild(div);
-
-  div.addEventListener("pointerenter", () => {
-    div.style.background = "red";
-  });
-}
+    div.addEventListener("pointerenter", () => {
+      div.style.background = "red";
+    });
+  }
+};
