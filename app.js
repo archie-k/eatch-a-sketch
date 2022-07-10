b65c4fb2c099e90b600cbe0d7f7cd7aa8e16f1d9
Artur <artur.krupa@icloud.com>
2022-07-10 23:52:57 +0200

    add div remover

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
+// DOM selectros
 const divContainer = document.querySelector(".grid-container");
 const sizeButton = document.querySelector(".grid-size-select");
+
 sizeButton.addEventListener("click", () => {
   let numberOfGrid = prompt("how big is tyour grid");
   renderCanvas(numberOfGrid ** 2);
+  clearCanvas();
 });
 
 let renderCanvas = (size) => {
   for (let i = 0; i < size; i++) {
-    let div = document.createElement("div");
+    div = document.createElement("div");
     divContainer.appendChild(div);
 
     div.addEventListener("pointerenter", () => {
       div.style.background = "red";
     });
   }
+};
+
+let clearCanvas = () => {
+  divContainer.removeChild(div);
+  console.log("test");
 };
